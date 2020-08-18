e59bc5cdbbe4bb473b314cead063d186b6bf4dad
kolbasik <sergii.kolbasin@albelli.com>
2020-08-18 17:31:51 +0200

    Add boto3 as dependency

--- zest.py
+++ zest.py
@@ -1,7 +1,7 @@
 import os, importlib, argparse
 
 ZEST_NAME = 'zest'
-ZEST_VERSION = '0.0.5'
+ZEST_VERSION = '0.0.6'
 ZEST_GIT = 'https://github.com/kolbasik/zest'
 
 parser = argparse.ArgumentParser(
@@ -18,14 +18,10 @@ args = parser.parse_args()
 # https://stackoverflow.com/questions/273192/how-can-i-safely-create-a-nested-directory
 if not os.path.exists(args.zwd):
   def download(url, file_name):
-    try: # 2.7
-      import urllib
-      urllib.urlretrieve(url, file_name)
-    except AttributeError: # 3.8
-      import urllib.request, shutil
-      with open(file_name, 'wb') as file:
-        with urllib.request.urlopen(url) as response:
-          shutil.copyfileobj(response, file)
+    import urllib.request, shutil
+    with open(file_name, 'wb') as file:
+      with urllib.request.urlopen(url) as response:
+        shutil.copyfileobj(response, file)
 
   def unzip(zip_file_name, dest):
     import zipfile
@@ -33,10 +29,20 @@ if not os.path.exists(args.zwd):
       zip_file.extractall(dest)
       return zip_file.namelist()[0]
 
+  def install(package):
+    # https://stackoverflow.com/questions/12332975/installing-python-module-within-code
+    try:
+      importlib.import_module(package)
+    except ImportError as e:
+      if args.verbose: print('The %s installing...' % package)
+      import sys, subprocess
+      subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
   zest_zip = '%s.zip' % ZEST_NAME
-  download('%s/archive/%s.zip' % (ZEST_GIT, os.getenv("ZEST_VERSION", ZEST_VERSION)), zest_zip)
+  download('%s/archive/%s.zip' % (ZEST_GIT, os.getenv('ZEST_VERSION', ZEST_VERSION)), zest_zip)
   os.rename(unzip(zest_zip, '.'), args.zwd)
   os.remove(zest_zip)
+  install('boto3')
 
 try:
   imported_command = importlib.import_module(
@@ -44,5 +50,6 @@ try:
     package='%s.commands' % (args.zwd if args.zwd != '.' else '')
   )
   imported_command.invoke(parser)
-except ModuleNotFoundError:
+except ModuleNotFoundError as e:
+  if args.verbose: print(repr(e))
   print('Unknown command: %s' % args.command)
